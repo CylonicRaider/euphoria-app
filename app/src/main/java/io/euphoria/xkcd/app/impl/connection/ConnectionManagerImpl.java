8f3dde4229556ca21678afa46733f1cc7e18c930
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-02-24 19:28:34 +0100

    Commence implementation classes

---
+++ app/src/main/java/io/euphoria/xkcd/app/impl/connection/ConnectionManagerImpl.java
@@ -0,0 +1,20 @@
+package io.euphoria.xkcd.app.impl.connection;
+
+import io.euphoria.xkcd.app.connection.Connection;
+import io.euphoria.xkcd.app.connection.ConnectionManager;
+
+/** Created by Xyzzy on 2017-02-24. */
+
+public class ConnectionManagerImpl implements ConnectionManager {
+
+    @Override
+    public Connection getConnection(String roomName) {
+        throw new AssertionError("Not implemented");
+    }
+
+    @Override
+    public Connection connect(String roomName) {
+        throw new AssertionError("Not implemented");
+    }
+
+}
---
+++ app/src/main/java/io/euphoria/xkcd/app/impl/data/RoomManagerImpl.java
@@ -0,0 +1,15 @@
+package io.euphoria.xkcd.app.impl.data;
+
+import io.euphoria.xkcd.app.data.Room;
+import io.euphoria.xkcd.app.data.RoomManager;
+
+/** Created by Xyzzy on 2017-02-24. */
+
+public class RoomManagerImpl implements RoomManager {
+
+    @Override
+    public Room getRoom(String name) {
+        throw new AssertionError("Not implemented");
+    }
+
+}
