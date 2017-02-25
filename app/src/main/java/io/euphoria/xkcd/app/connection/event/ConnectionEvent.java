6f680ecd98f01511cd4c6f6995106349a3cc40e4
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-02-25 22:41:27 +0100

    Add event mapping to docs

--- app/src/main/java/io/euphoria/xkcd/app/connection/event/ConnectionEvent.java
+++ app/src/main/java/io/euphoria/xkcd/app/connection/event/ConnectionEvent.java
@@ -4,7 +4,22 @@ import io.euphoria.xkcd.app.connection.Connection;
 
 /** Created by Xyzzy on 2017-02-24. */
 
-/** A message received by a Connection wrapped into the event concept */
+/**
+ * A message received by a Connection wrapped into the event concept
+ *
+ * Event/reply-event mapping (omitted ones are not implemented):
+ * join-event        -> PresenceChangeEvent
+ * network-event     -> PresenceChangeEvent
+ * nick-event        -> NickChangeEvent
+ * part-event        -> PresenceChangeEvent
+ * send-event        -> MessageEvent
+ * snapshot-event    -> LogEvent & PresenceChangeEvent
+ * get-message-reply -> LogEvent
+ * log-reply         -> LogEvent
+ * nick-reply        -> NickChangeEvent
+ * send-reply        -> MessageEvent
+ * who-reply         -> PresenceChangeEvent
+ */
 public interface ConnectionEvent {
 
     /** The connection this event originated from */
