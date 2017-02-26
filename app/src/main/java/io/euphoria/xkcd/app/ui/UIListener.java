d83a935b1d11dd927e2749c553d4fc8167cdfcea
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-02-26 18:52:07 +0100

    Add room UI events
    They do indeed mirror connection actions, much like UI actions
    (loosely) mirror connection events.

---
+++ app/src/main/java/io/euphoria/xkcd/app/ui/UIListener.java
@@ -0,0 +1,25 @@
+package io.euphoria.xkcd.app.ui;
+
+import io.euphoria.xkcd.app.ui.event.LogRequestEvent;
+import io.euphoria.xkcd.app.ui.event.MessageSendEvent;
+import io.euphoria.xkcd.app.ui.event.NewNickEvent;
+import io.euphoria.xkcd.app.ui.event.RoomSwitchEvent;
+
+/** Created by Xyzzy on 2017-02-26. */
+
+/* Listener interface for UI events */
+public interface UIListener {
+
+    /* The user intends to change the nickname */
+    void onNewNick(NewNickEvent evt);
+
+    /* The user intends to send a message */
+    void onMessageSend(MessageSendEvent evt);
+
+    /* The user wishes to see more room logs */
+    void onLogRequest(LogRequestEvent evt);
+
+    /* The user intends to change to another room */
+    void onRoomSwitch(RoomSwitchEvent evt);
+
+}
---
+++ app/src/main/java/io/euphoria/xkcd/app/ui/event/LogRequestEvent.java
@@ -0,0 +1,10 @@
+package io.euphoria.xkcd.app.ui.event;
+
+/** Created by Xyzzy on 2017-02-26. */
+
+/* Event encapsulating the desire to see more room logs */
+public interface LogRequestEvent extends UIEvent {
+
+    /* Blank */
+
+}
