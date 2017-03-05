ab0770626a569f1a13f0b2fbdd0715bfbdfd740d
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-03-05 19:56:49 +0100

    Add closing events

---
+++ app/src/main/java/io/euphoria/xkcd/app/connection/event/CloseEvent.java
@@ -0,0 +1,10 @@
+package io.euphoria.xkcd.app.connection.event;
+
+/** Created by Xyzzy on 2017-03-05. */
+
+/* An event informing about the connection having been closed */
+public interface CloseEvent extends ConnectionEvent {
+
+    /* Blank */
+
+}
---
+++ app/src/main/java/io/euphoria/xkcd/app/ui/event/CloseEvent.java
@@ -0,0 +1,10 @@
+package io.euphoria.xkcd.app.ui.event;
+
+/** Created by Xyzzy on 2017-03-05. */
+
+/* Event encapsulating the wish to close the room */
+public interface CloseEvent extends UIEvent {
+
+    /* Blank */
+
+}
