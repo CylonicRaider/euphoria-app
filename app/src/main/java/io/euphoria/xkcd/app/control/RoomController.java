df63b47b38daab14dd1238e60fc7069be6a91a14
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-03-19 20:52:53 +0000

    Allow passing context to RoomController
    Because Android.

--- app/src/main/java/io/euphoria/xkcd/app/control/RoomController.java
+++ app/src/main/java/io/euphoria/xkcd/app/control/RoomController.java
@@ -1,17 +1,25 @@
 package io.euphoria.xkcd.app.control;
 
+import android.content.Context;
+
 import io.euphoria.xkcd.app.ui.RoomUIManager;
 
 /** Created by Xyzzy on 2017-03-19. */
 
 public class RoomController {
     private final RoomUIManager manager;
+    private final Context context;
 
-    public RoomController(RoomUIManager mgr) {
+    public RoomController(RoomUIManager mgr, Context ctx) {
         this.manager = mgr;
+        this.context = ctx;
     }
 
     public RoomUIManager getManager() {
         return manager;
+    }
+
+    public Context getContext() {
+        return context;
     }
 }
