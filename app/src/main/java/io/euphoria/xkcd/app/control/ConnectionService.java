269bbb2e0f1772de11a0aa2db231431d276a5e03
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-03-19 22:38:27 +0000

    Implement binding to connection service

--- app/src/main/java/io/euphoria/xkcd/app/control/ConnectionService.java
+++ app/src/main/java/io/euphoria/xkcd/app/control/ConnectionService.java
@@ -2,15 +2,24 @@ package io.euphoria.xkcd.app.control;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
 /** Created by Xyzzy on 2017-03-19. */
 
 public class ConnectionService extends Service {
+    public class CBinder extends Binder {
+        public ConnectionService getService() {
+            return ConnectionService.this;
+        }
+    }
+
+    private final CBinder BINDER = new CBinder();
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return BINDER;
     }
 }
--- app/src/main/java/io/euphoria/xkcd/app/control/RoomController.java
+++ app/src/main/java/io/euphoria/xkcd/app/control/RoomController.java
@@ -1,7 +1,10 @@
 package io.euphoria.xkcd.app.control;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 
 import io.euphoria.xkcd.app.ui.RoomUIManager;
 
@@ -10,6 +13,8 @@ import io.euphoria.xkcd.app.ui.RoomUIManager;
 public class RoomController {
     private final RoomUIManager manager;
     private final Context context;
+    private ConnectionService service;
+    private ServiceConnection connection;
 
     public RoomController(RoomUIManager mgr, Context ctx) {
         this.manager = mgr;
@@ -26,10 +31,34 @@ public class RoomController {
     }
 
     private void start() {
-        context.startService(new Intent(context, ConnectionService.class));
+        Intent intent = new Intent(context, ConnectionService.class);
+        context.startService(intent);
+        connection = new ServiceConnection() {
+            private ConnectionService service;
+
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                // Wheee! I should name even more variables like that!
+                this.service = ((ConnectionService.CBinder) service).getService();
+                RoomController.this.service = this.service;
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                // Prevent race condition.
+                if (RoomController.this.service == this.service)
+                    RoomController.this.service = null;
+            }
+        };
+        // Service already created.
+        context.bindService(intent, connection, Context.BIND_AUTO_CREATE);
     }
 
     public void shutdown() {
-        throw new AssertionError("Not implemented");
+        if (service != null) {
+            context.unbindService(connection);
+            service = null;
+            connection = null;
+        }
     }
 }
