67a951c0b51bcc1b0440df501fd7a14e9808c9d8
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-03-20 19:00:33 +0000

    Create connection manager in service

--- app/src/main/java/io/euphoria/xkcd/app/control/ConnectionService.java
+++ app/src/main/java/io/euphoria/xkcd/app/control/ConnectionService.java
@@ -6,6 +6,9 @@ import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
+import io.euphoria.xkcd.app.connection.ConnectionManager;
+import io.euphoria.xkcd.app.impl.connection.ConnectionManagerImpl;
+
 /** Created by Xyzzy on 2017-03-19. */
 
 public class ConnectionService extends Service {
@@ -16,10 +19,21 @@ public class ConnectionService extends Service {
     }
 
     private final CBinder BINDER = new CBinder();
+    private ConnectionManager mgr;
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return BINDER;
+    }
+
+    @Override
+    public void onCreate() {
+        mgr = ConnectionManagerImpl.getInstance();
+    }
+
+    @Override
+    public void onDestroy() {
+        mgr.shutdown();
     }
 }
