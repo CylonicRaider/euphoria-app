7063df4d4df7bbb29d2ab92d7b3877ce93d06d37
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-05-03 19:55:05 +0000

    Implement convenient packet sending

--- app/src/main/java/io/euphoria/xkcd/app/impl/connection/EuphoriaWebSocketClient.java
+++ app/src/main/java/io/euphoria/xkcd/app/impl/connection/EuphoriaWebSocketClient.java
@@ -1,7 +1,11 @@
 package io.euphoria.xkcd.app.impl.connection;
 
+import android.util.Log;
+
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.URI;
 
@@ -83,6 +87,28 @@ public class EuphoriaWebSocketClient extends WebSocketClient {
     @Override
     public void onError(Exception ex) {
         parent.submitEvent(new CloseEventImpl(false));
+    }
+
+    public int sendObject(String type, Object... data) {
+        if (data.length == 0) data = null;
+        int seq = parent.sequence();
+        try {
+            send(buildJSONObject("type", type, "id", seq, "data", buildJSONObject(data)).toString());
+        } catch (JSONException exc) {
+            Log.e("EuphoriaWebSocketClient", "Exception while serializing JSON", exc);
+        }
+        return seq;
+    }
+
+    private static JSONObject buildJSONObject(Object... data) throws JSONException {
+        if (data.length % 2 != 0) throw new IllegalArgumentException("Invalid JSON object construction shortcut");
+        JSONObject ret = new JSONObject();
+        for (int i = 0; i < data.length; i += 2) {
+            if (! (data[i] instanceof String)) throw new JSONException("Object key is not a string");
+            if (data[i + 1] == null) continue;
+            ret.put((String) data[i], data[i + 1]);
+        }
+        return ret;
     }
 
 }
