f7f48e6395a3699418210073880be2ecc10cea7e
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-09-28 21:21:01 +0000

    Use margins for message indenting

--- app/src/main/java/io/euphoria/xkcd/app/impl/ui/InputBarView.java
+++ app/src/main/java/io/euphoria/xkcd/app/impl/ui/InputBarView.java
@@ -1,13 +1,9 @@
 package io.euphoria.xkcd.app.impl.ui;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
@@ -28,7 +24,7 @@ public class InputBarView extends RelativeLayout {
 
     }
 
-    private final MarginLayoutParams defaultParams;
+    private final MarginLayoutParams defaultLayoutParams;
     private EditText nickEntry;
     private EditText messageEntry;
     private MessageTree tree;
@@ -36,19 +32,7 @@ public class InputBarView extends RelativeLayout {
 
     public InputBarView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        // FIXME only resolving some of the attributes; move into custom layout manager?
-        // Here be dragons
-        int[] indices = new int[] {android.R.attr.layout_width, android.R.attr.layout_height,
-                android.R.attr.layout_marginTop, android.R.attr.layout_marginBottom};
-        TypedArray values = context.obtainStyledAttributes(attrs, indices);
-        // Yes, WRAP_CONTENT *is* already in the right domain, thank you.
-        @SuppressWarnings("ResourceType")
-        ViewGroup.LayoutParams core = new ViewGroup.LayoutParams(
-                values.getLayoutDimension(0, LayoutParams.MATCH_PARENT),
-                values.getLayoutDimension(1, LayoutParams.WRAP_CONTENT));
-        defaultParams = new MarginLayoutParams(core);
-        defaultParams.setMargins(0, values.getDimensionPixelSize(2, 0), 0, values.getDimensionPixelSize(3, 0));
-        values.recycle();
+        defaultLayoutParams = MessageView.getDefaultMargins(context, attrs);
     }
 
     @Override
@@ -133,19 +117,16 @@ public class InputBarView extends RelativeLayout {
     public void setIndent(int indent) {
         MarginLayoutParams lp = (MarginLayoutParams) getLayoutParams();
         if (lp == null) {
-            lp = new MarginLayoutParams(defaultParams);
+            lp = new MarginLayoutParams(defaultLayoutParams);
             setLayoutParams(lp);
         }
-        lp.leftMargin = MessageView.computeIndentWidth(getContext(), indent);
-        if (VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN_MR1) {
-            lp.setMarginStart(lp.leftMargin);
-        }
+        MessageView.setMarginForIndent(getContext(), lp, indent);
         if (indent == 0) {
-            lp.topMargin = defaultParams.topMargin + defaultParams.bottomMargin;
+            lp.topMargin = defaultLayoutParams.topMargin + defaultLayoutParams.bottomMargin;
             lp.bottomMargin = 0;
         } else {
-            lp.topMargin = defaultParams.topMargin;
-            lp.bottomMargin = defaultParams.bottomMargin;
+            lp.topMargin = defaultLayoutParams.topMargin;
+            lp.bottomMargin = defaultLayoutParams.bottomMargin;
         }
     }
 
--- app/src/main/java/io/euphoria/xkcd/app/impl/ui/MessageView.java
+++ app/src/main/java/io/euphoria/xkcd/app/impl/ui/MessageView.java
@@ -1,9 +1,12 @@
 package io.euphoria.xkcd.app.impl.ui;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -21,11 +24,13 @@ public class MessageView extends RelativeLayout {
 
     private static final int PADDING_PER_INDENT = 15;
 
+    private final MarginLayoutParams defaultLayoutParams;
     private MessageTree message = null;
     private boolean established = false;
 
-    public MessageView(Context ctx, AttributeSet attrs) {
-        super(ctx, attrs);
+    public MessageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        defaultLayoutParams = getDefaultMargins(context, attrs);
     }
 
     public void setMessage(@NonNull MessageTree message) {
@@ -43,7 +48,12 @@ public class MessageView extends RelativeLayout {
         TextView nickLbl = (TextView) findViewById(R.id.nick_lbl);
         TextView contentLbl = (TextView) findViewById(R.id.content_lbl);
         TextView collapseLbl = (TextView) findViewById(R.id.collapse_lbl);
-        this.setPadding(computeIndentWidth(getContext(), message.getIndent()), 0, 0, 0);
+        MarginLayoutParams lp = (MarginLayoutParams) getLayoutParams();
+        if (lp == null) {
+            lp = new MarginLayoutParams(defaultLayoutParams);
+            setLayoutParams(lp);
+        }
+        setMarginForIndent(getContext(), lp, message.getIndent());
         if (message != null && message.getMessage() != null) {
             contentLbl.setText(message.getMessage().getContent());
             SessionView sender = message.getMessage().getSender();
@@ -92,6 +102,44 @@ public class MessageView extends RelativeLayout {
 
     public static int computeIndentWidth(Context ctx, int indent) {
         return indent * dpToPx(ctx, PADDING_PER_INDENT);
+    }
+
+    @SuppressWarnings("ResourceType")
+    static MarginLayoutParams getDefaultMargins(Context context, AttributeSet attrs) {
+        // FIXME only resolving some of the attributes; move into custom layout manager?
+        // Here be dragons
+        int[] indices;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            indices = new int[] {android.R.attr.layout_width, android.R.attr.layout_height,
+                    android.R.attr.layout_marginLeft, android.R.attr.layout_marginTop,
+                    android.R.attr.layout_marginRight, android.R.attr.layout_marginBottom,
+                    android.R.attr.layout_marginStart, android.R.attr.layout_marginEnd};
+        } else {
+            indices = new int[] {android.R.attr.layout_width, android.R.attr.layout_height,
+                    android.R.attr.layout_marginLeft, android.R.attr.layout_marginTop,
+                    android.R.attr.layout_marginRight, android.R.attr.layout_marginBottom};
+        }
+        TypedArray values = context.obtainStyledAttributes(attrs, indices);
+        ViewGroup.LayoutParams core = new ViewGroup.LayoutParams(
+                values.getLayoutDimension(0, LayoutParams.MATCH_PARENT),
+                values.getLayoutDimension(1, LayoutParams.WRAP_CONTENT));
+        MarginLayoutParams ret = new MarginLayoutParams(core);
+        ret.setMargins(values.getDimensionPixelSize(2, 0), values.getDimensionPixelSize(3, 0),
+                values.getDimensionPixelSize(4, 0), values.getDimensionPixelSize(5, 0));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            ret.setMarginStart(values.getDimensionPixelSize(6, 0));
+            ret.setMarginStart(values.getDimensionPixelSize(7, 0));
+        }
+        values.recycle();
+        return ret;
+    }
+
+    static void setMarginForIndent(Context context, MarginLayoutParams params, int indent) {
+        int l = computeIndentWidth(context, indent);
+        params.leftMargin = l;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            params.setMarginStart(l);
+        }
     }
 
 }
