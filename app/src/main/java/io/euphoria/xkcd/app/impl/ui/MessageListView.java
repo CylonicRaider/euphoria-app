fa77e17d80d393112fefbf36cd5622c57855562f
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-10-02 16:35:14 +0000

    Commence separate message list view class

---
+++ app/src/main/java/io/euphoria/xkcd/app/impl/ui/MessageListView.java
@@ -0,0 +1,18 @@
+package io.euphoria.xkcd.app.impl.ui;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/** Created by Xyzzy on 2017-10-02. */
+
+public class MessageListView extends RecyclerView {
+
+    private static final String TAG = "MessageListView";
+
+    public MessageListView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
