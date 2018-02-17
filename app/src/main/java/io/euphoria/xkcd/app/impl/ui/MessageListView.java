3a622ee67e736d2c6f4875ad19fcebe7bf74580f
CylonicRaider <CylonicRaider@users.noreply.github.com>
2018-02-17 22:45:45 +0000

    Disable animations for now
    Proper indent line support would require creating an entire custom
    animator. Euphoria doesn't have fancy animations, either. I know those
    are no excuses.

--- app/src/main/java/io/euphoria/xkcd/app/impl/ui/MessageListView.java
+++ app/src/main/java/io/euphoria/xkcd/app/impl/ui/MessageListView.java
@@ -3,7 +3,6 @@ package io.euphoria.xkcd.app.impl.ui;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
@@ -170,9 +169,9 @@ public class MessageListView extends RecyclerView {
         LinearLayoutManager lm = new LinearLayoutManager(context);
         lm.setStackFromEnd(true);
         setLayoutManager(lm);
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
-            setItemAnimator(null);
-        }
+        // FIXME: Re-add animations.
+        //if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB)
+        setItemAnimator(null);
     }
 
     @Override
