0942034718364c499127aeaf6701572ccf4a0e8f
CylonicRaider <CylonicRaider@users.noreply.github.com>
2020-07-08 22:11:33 +0000

    Add comparison methods to Version class

--- app/src/main/java/io/euphoria/xkcd/app/updates/Version.java
+++ app/src/main/java/io/euphoria/xkcd/app/updates/Version.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 /** Created by Xyzzy on 2020-07-08. */
 
-public class Version {
+public class Version implements Comparable<Version> {
 
     private static final Pattern VALID_VERSION_STRING = Pattern.compile("^[1-9][0-9]*(\\.[1-9][0-9]*)*$");
 
@@ -12,6 +12,35 @@ public class Version {
 
     public Version(int... fields) {
         this.fields = fields.clone();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return ((other instanceof Version) && (compareTo((Version) other) == 0));
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        for (int f : fields) {
+            hash = hash * 31 + f;
+        }
+        return hash;
+    }
+
+    @Override
+    public int compareTo(Version other) {
+        int fc = getFieldCount(), ofc = other.getFieldCount();
+        for (int i = 0;; i++) {
+            if (i == fc) {
+                if (i == ofc) return 0;
+                return -1;
+            } else if (i == ofc) {
+                return 1;
+            } else if (getField(i) != other.getField(i)) {
+                return getField(i) - other.getField(i);
+            }
+        }
     }
 
     public int[] getFields() {
