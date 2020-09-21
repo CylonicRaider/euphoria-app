c6d7e9fd2db9287203c33be48852051b2f1cfd83
N00bySumairu <N00bySumairu@users.noreply.github.com>
2020-09-21 02:01:10 +0200

    Add cookie settings and settings helper class

---
+++ app/src/main/java/io/euphoria/xkcd/app/Settings.java
@@ -0,0 +1,58 @@
+package io.euphoria.xkcd.app;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+
+import java.net.HttpCookie;
+import java.util.List;
+
+public class Settings {
+    private static final String KEY_CONTINUE_PREV_SESSION = "continue_prev_session";
+    private static final String KEY_SESSION_COOKIE = "session_cookie";
+
+    private final SharedPreferences preferences;
+
+    public Settings(Context context) {
+        this.preferences = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    /**
+     * Check in the apps settings if the previous session should be continued.
+     *
+     * @return <code>true</code> if the previous session should be continued, <code>false</code> otherwise.
+     */
+    public boolean shouldContinuePrevSession() {
+        return preferences.getBoolean(KEY_CONTINUE_PREV_SESSION, false);
+    }
+
+    /**
+     * Retrieve the stored session cookie, if one was stored previously.
+     *
+     * @return The retrieved session cookie, or <code>null</code> if none was stored previously.
+     */
+    @Nullable
+    public HttpCookie getSessionCookie() {
+        String cookieStr = preferences.getString(KEY_SESSION_COOKIE, null);
+        if (cookieStr == null) return null;
+        List<HttpCookie> parsedCookies = HttpCookie.parse(cookieStr);
+        if (parsedCookies.size() != 1) return null;
+        return parsedCookies.get(0);
+    }
+
+    /**
+     * Store the current session cookie to be re-used for future session.
+     *
+     * @param sessionCookie The session cookie to store, or <code>null</code> to clear the currently stored session cookie.
+     */
+    public void setSessionCookie(@Nullable HttpCookie sessionCookie) {
+        SharedPreferences.Editor prefEditor = preferences.edit();
+        if (sessionCookie == null) {
+            prefEditor.remove(KEY_SESSION_COOKIE);
+        } else {
+            prefEditor.putString(KEY_SESSION_COOKIE, sessionCookie.toString());
+        }
+        prefEditor.apply();
+    }
+}
