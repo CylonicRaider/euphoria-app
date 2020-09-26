96fe1e6c5170b21b86efafb7cc87249f1dac4c69
CylonicRaider <CylonicRaider@users.noreply.github.com>
2020-09-26 21:52:05 +0000

    More minor whitespace and import adjustments

--- app/src/main/java/io/euphoria/xkcd/app/SettingsActivity.java
+++ app/src/main/java/io/euphoria/xkcd/app/SettingsActivity.java
@@ -1,18 +1,19 @@
 package io.euphoria.xkcd.app;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.widget.Toolbar;
 
 public class SettingsActivity extends AppCompatActivity {
+
     public static class SettingsFragment extends PreferenceFragmentCompat {
+
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.preferences, rootKey);
         }
+
     }
 
     @Override
@@ -27,4 +28,5 @@ public class SettingsActivity extends AppCompatActivity {
                 .replace(R.id.settings_container, new SettingsFragment())
                 .commit();
     }
+
 }
