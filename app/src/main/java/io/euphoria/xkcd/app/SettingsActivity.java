3011707d72cc2cc82399b6ee957708dd011e6e07
CylonicRaider <CylonicRaider@users.noreply.github.com>
2020-09-29 17:31:35 +0000

    Add confirmation dialog to cookie deletion
    This should conclude this feature.
    
    Closes #4.

--- app/src/main/java/io/euphoria/xkcd/app/SettingsActivity.java
+++ app/src/main/java/io/euphoria/xkcd/app/SettingsActivity.java
@@ -1,6 +1,8 @@
 package io.euphoria.xkcd.app;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
@@ -19,7 +21,18 @@ public class SettingsActivity extends AppCompatActivity {
             deletePref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
-                    settings.setSessionCookie(null);
+                    new AlertDialog.Builder(getContext())
+                            .setTitle(getString(R.string.setting_delete_cookie_title))
+                            .setMessage(R.string.setting_delete_cookie_confirmation)
+                            .setPositiveButton(R.string.setting_delete_cookie_commit,
+                                    new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    settings.setSessionCookie(null);
+                                }
+                            })
+                            .setNegativeButton(R.string.cancel, null)
+                            .show();
                     return true;
                 }
             });
